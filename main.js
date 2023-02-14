348055877dfe07f21387f02bf456ddc17006e821
ChaevelyDao <daominhduc8010@gmail.com>
2023-02-15 02:40:11 +0700

    fix

--- main.js
+++ main.js
@@ -1,29 +1,30 @@
-// let slideIndex = 1;
-// showSlides(slideIndex);
+document.addEventListener('mouseover',(e) => {
+    console.log(e)
+    const mouseX = e.clientX 
+    const mouseY = e.clientY 
 
-// function plusSlides(n) {
-//   showSlides(slideIndex += n);
-// }
+    const anchor = document.getElementById('anchor')
+    const rekt = anchor.getBoundingClientRect();
+    const anchorX = rekt.left + rekt.width /2;
+    const anchorY = rekt.top + rekt.height /2;
 
-// function currentSlide(n) {
-//   showSlides(slideIndex = n);
-// }
+    const angleDeg = angle(mouseX,mouseY,anchorX,anchorY);
 
-// function showSlides(n) {
-//   let i;
-//   let slides = document.getElementsByClassName("mySlides");
-//   let dots = document.getElementsByClassName("dot");
-//   if (n > slides.length) {slideIndex = 1}
-//   if (n < 1) {slideIndex = slides.length}
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";
-//   }
-//   for (i = 0; i < dots.length; i++) {
-//     dots[i].className = dots[i].className.replace(" active", "");
-//   }
-//   slides[slideIndex-1].style.display = "block";
-//   dots[slideIndex-1].className += " active";
-// }
+    console.log(angleDeg)
+
+    const eyes = document.querySelectorAll('.eye')
+    eyes.forEach(eye => {
+        eye.style.transform = `rotate(${90 + angleDeg}deg)`;
+    })
+})
+
+function angle(cx,cy,ex,ey){
+    const dy = ey - cy;
+    const dx = ex - cx;
+    const rad = Math.atan2(dy,dx);
+    const deg = rad * 180 / Math.PI;
+    return deg;
+}
 
 var timeOut = 2000;
 var slideIndex = 0;
